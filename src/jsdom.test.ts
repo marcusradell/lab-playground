d33bee033f32de735ecd8bb8360c2f3ef503bfd6
Marcus Rådell <marcus@radell.net>
2024-10-10 10:48:28 +0200

    get all paragraphs

--- src/jsdom.test.ts
+++ src/jsdom.test.ts
@@ -1,4 +1,4 @@
-import { equal } from "assert/strict";
+import { deepEqual, equal } from "assert/strict";
 import { test } from "node:test";
 import { JSDOM } from "jsdom";
 
@@ -6,4 +6,14 @@ test("select paragraph and access textContent", () => {
   const dom = new JSDOM(`<!DOCTYPE html><p>Hi!</p>`);
 
   equal(dom.window.document.querySelector("p")!.textContent, "Hi!");
+});
+
+// NOTE: show class a better way to test this
+test("get all paragraphs", () => {
+  const dom = new JSDOM(`
+    <!DOCTYPE html><p>Hi!</p><p>Hidyhow!</p>
+    `);
+  const result = dom.window.document.querySelectorAll("p")[0];
+
+  deepEqual(result, dom.window.document.createElement("p"));
 });
